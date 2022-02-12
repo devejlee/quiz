9fbfa60abf2e6cf84274cc99adf2f349663fcc8c
Elijah Lee <develijahlee@gmail.com>
2022-02-12 17:44:01 +0900

    add handleClick, showNextQuestion logic

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,24 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import styles from '../styles/quiz.module.scss'
 
 const Quiz: NextPage = () => {
   const [data, setData] = useState([] as any[]);
   const [questionCount, setQuestionCount] = useState(0)
   const [answerCount, setAnswerCount] = useState(0)
+  const [answered, setAnswered] = useState(false)
+  const correctAnswerRef = useRef(null)
 
   useEffect(() => {
     const results = JSON.parse(window.localStorage.getItem('results') || '{}')
     setData(results)
   }, []);
 
-  const handleClick = (value: boolean): void => {
-    setQuestionCount(questionCount => questionCount + 1)
-    if (value) {
+  const handleClick = (value: boolean, e: any): void => {
+    setAnswered(true)
+    if (value && !answered) {
+      e.classList.add(styles.correct)
       setAnswerCount(answerCount => answerCount + 1)
+    } else if (!value && !answered) {
+      e.classList.add(styles.incorrect)
     }
+  }
+
+  const showNextQuestion = (): void => {
+    if (answered) {
+      setQuestionCount(questionCount => questionCount + 1)
+      setAnswered(false)
+    } else {
+      alert('choose an answer')
+    }
+    correctAnswerRef.current?.classList.remove(styles.correct)
   }
 
   return (
@@ -37,17 +52,18 @@ const Quiz: NextPage = () => {
           <div key={index}>
             <p dangerouslySetInnerHTML={{ __html: d.question }} />
             {d.incorrect_answers.map((d: string) => (
-              <p key={d} dangerouslySetInnerHTML={{ __html: d }} onClick={() => handleClick(false)} />
+              <p key={d} dangerouslySetInnerHTML={{ __html: d }} onClick={(e) => handleClick(false, e.target)} />
             ))}
-            <p dangerouslySetInnerHTML={{ __html: d.correct_answer }} onClick={() => handleClick(true)} />
+            <p ref={correctAnswerRef} dangerouslySetInnerHTML={{ __html: d.correct_answer }} onClick={(e) => handleClick(true, e.target)} />
           </div>
         ))}
 
-        {questionCount !== data.length && (
-          <p>questionCount: {questionCount + 1} out of {data.length}</p>
-        )}
-
-        {questionCount === data.length && (
+        {questionCount !== data.length ? (
+          <>
+            <p>questionCount: {questionCount + 1} out of {data.length}</p>
+            <button onClick={showNextQuestion}>next</button>
+          </>
+        ) : (
           <p>answerCount: {answerCount}</p>
         )}
 
